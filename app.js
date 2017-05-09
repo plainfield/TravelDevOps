00f24cfe5b4649b0c3c909ac46f34a072aed15c6
Jacky <maozy@cn.ibm.com>
2017-05-09 17:24:59 +0800

    change to black style

--- app.js
+++ app.js
@@ -6,6 +6,14 @@
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
+
+//fix the issue in ports module
+var homedir = "HOME";
+if(process.platform === "win32") {
+   homedir = "USERPROFILE";
+}
+if(process.env[homedir]==undefined) process.env[homedir]=__dirname;
+
 var express = require('express');
 
 // cfenv provides access to your Cloud Foundry environment
@@ -22,7 +30,7 @@ app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+app.listen(appEnv.port || 3000, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
