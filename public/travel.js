a6f247c32a4d9bbb21f9eaab7b99f9a2f4f7e6cd
Jacky <maozy@cn.ibm.com>
2017-05-13 18:43:45 +0800

    changed iteration 1 with new style

--- public/travel.js
+++ public/travel.js
@@ -6,21 +6,21 @@ $(document).ready(function () {
     $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
 
         var $target = $(e.target);
-    
+        console.log("111111");
         if ($target.parent().hasClass('disabled')) {
             return false;
         }
     });
 
     $(".next-step").click(function (e) {
-
+    	console.log("2222");
         var $active = $('.wizard .wizard-inner .nav-tabs li.active');
         $active.next().removeClass('disabled');
         nextTab($active);
 
     });
     $(".prev-step").click(function (e) {
-
+    	console.log("33333");
         var $active = $('.wizard .nav-tabs li.active');
         prevTab($active);
 
@@ -28,8 +28,10 @@ $(document).ready(function () {
 });
 
 function nextTab(elem) {
+	console.log("4444");
     $(elem).next().find('a[data-toggle="tab"]').click();
 }
 function prevTab(elem) {
+	console.log("5555");
     $(elem).prev().find('a[data-toggle="tab"]').click();
 }
