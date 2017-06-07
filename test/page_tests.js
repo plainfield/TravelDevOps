24a18c2cbb3eb4307a98e08cd96f4ddd18ee7a2e
Jacky <maozy@cn.ibm.com>
2017-06-07 17:34:03 +0800

    test works

--- test/page_tests.js
+++ test/page_tests.js
@@ -6,7 +6,7 @@ var agent = supertest.agent(app);
 describe ("test API services:", function() {
     this.timeout(30000);
     it ("should not get home page", function (done) {
-        agent.get('/pubic/index.html')
+        agent.get('/index.html')
         .expect(200)
         .end(function(err, res){
             if (err) return done(err);
